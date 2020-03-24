48e38a4e24c7da2d361aba67871f6dd0a16aa1d6
Nikolina Bućan <bucan DOT nikolina AT yahoo DOT com>
2020-03-24 22:03:01 +0100

    Program will not crash if there is no informations for interpolation. Instead of that the program will show notification which informations are missing.

--- skriptaFotosop.py
+++ skriptaFotosop.py
@@ -13,27 +13,49 @@ label=Label(text="Select 2 colors to interpolate between them", padx=50, pady=20
 #canvas.grid(row=1, column=3, )
 #canvas["bg"]="green"
 
+alist = []
+print("a")
 def chooseColor():
 	#global color
-	color = askcolor()
+
 	#print(color)
-	label=Label(text="First color is:").grid(row=2, column=2)
-	canvas= tkinter.Canvas(width=30, height=30)
-	canvas.grid(row=3, column=2)
+  color = askcolor()
+  label=Label(text="First color is:").grid(row=2, column=2)
+  canvas= tkinter.Canvas(width=30, height=30)
+  canvas.grid(row=3, column=2)
 	#canvas.print(color)	
 	#canvas["bg"]="red"
-	canvas.configure(background=color[1])
+  canvas.configure(background=color[1])
+  global statusCheck
+  statusCheck1= True
+  print(str(statusCheck1))
+  alist.append(1)
+  
+
+ 
+
 
 def chooseColor2():
 	#global color2
+
 	color2 = askcolor()
-		#print(color)
+			#print(color)
 	label2=Label(text="Second color is:").grid(row=6, column=2)
 	canvas2= tkinter.Canvas(width=30, height=30)
 	canvas2.grid(row=7, column=2)
-		#canvas.print(color)	
-		#canvas["bg"]="red"
+			#canvas.print(color)	
+			#canvas["bg"]="red"
 	canvas2.configure(background=color2[1])
+	global statusCheck
+	statusCheck2= True
+	print(str(statusCheck2))
+	alist.append(2)
+	
+
+
+
+
+
 
 
 
@@ -49,46 +71,98 @@ label_number= Label(text="Enter the number of interpolations:").grid(row=9, colu
 e = Entry (root, width=10,  borderwidth=1 )
 e.grid(row=10, column=2, )
 
+def from_rgb(rgb):
+    """translates an rgb tuple of int to a tkinter friendly color code
+    """
+    return "#%02x%02x%02x" % rgb  
 
+x_list=[]
+i = 0
+while i<100:
+    x_list.append(i)
+    i += 1
+print(x_list)    
+
+labelMessage=Label(text="", width=50).grid(row=13, column=2)
 def interpolate():
-				top=Toplevel()
-				top.title("Color palette")
-				top.geometry("242x350")	
-				broj = e.get()
-				global broj2
-				broj2=int(broj)+2
-				#print(broj2)
-				if broj2>7:
-					labelProba=Label(text="ukupno "+ str(broj2)).grid(row=13, column=2)
-					for i in range(broj2//10):
-						for j in range(10):
+	
+	alist.append(3)
+	broj = e.get()
+	#brojI=int(broj)
+	#print(brojI)
+
+	if ( 1 in alist ) and ( 2 in alist ) and not len(e.get()) ==0: 
+					labelMessage=Label(text="", width=50).grid(row=13, column=2)
+					top=Toplevel()
+					top.title("Color palette")
+					top.geometry("242x350")	
+					broj = e.get()
+					global broj2
+					broj2=int(broj)+2
+					#print(broj2)
+					if broj2>9:
+						for i in range(broj2//10):
+							for j in range(10):
+								print("/", end="")
+								canvas= tkinter.Canvas(top, width=20, height=20)
+								canvas.grid(row=1+i, column=j)
+								#canvas.configure(background="red")
+								canvas.configure(background="red")	
+
+							
+						for k in range(broj2 % 10):
 							print("/", end="")
 							canvas= tkinter.Canvas(top, width=20, height=20)
-							canvas.grid(row=1+i, column=j)
+							canvas.grid(row=2+i, column=k)
 							#canvas.configure(background="red")
-							
-							canvas.configure(background="red")	
+							canvas.configure(background="red")
+					else:			
+						 for l in range(broj2):
+								print("/", end="")
+								canvas= tkinter.Canvas(top, width=20, height=20)
+								canvas.grid(row=1, column=l)
+								#canvas.configure(background="red")
+								canvas.configure(background="red")
 
+
+	elif not ( 1 in alist ) and not ( 2 in alist ) and  len(e.get()) ==0:
+			labelMessage=Label(text="You must enter all the information", width=50).grid(row=13, column=2)							
+	elif ( 1 in alist ) and not ( 2 in alist ) and len(e.get()) ==0:
+			labelMessage=Label(text="Select the second color and enter the number", width=50).grid(row=13, column=2)
+	elif ( 1 in alist ) and not ( 2 in alist ) and not len(e.get()) ==0:
+			labelMessage=Label(text="Select the second color", width=50).grid(row=13, column=2)
+	elif ( 1 in alist ) and ( 2 in alist ) and len(e.get()) ==0:
+			labelMessage=Label(text="Enter the number of interpolations", width=50).grid(row=13, column=2)
+
+
+	elif not ( 1 in alist ) and ( 2 in alist ) and not len(e.get()) ==0:
+			labelMessage=Label(text="Select the first color", width=50).grid(row=13, column=2)
+	elif not ( 1 in alist ) and ( 2 in alist ) and len(e.get()) ==0:
+			labelMessage=Label(text="Select the first color and enter the number ", width=50).grid(row=13, column=2)
+
+
+	elif not ( 1 in alist ) and not ( 2 in alist ) and not len(e.get()) ==0:
+			labelMessage=Label(text="Select the first and second colors", width=50).grid(row=13, column=2)		
+			
+	else:			
+			labelMessage=Label(text="You must enter an entire positive number", width=50).grid(row=13, column=2)
+			    	
 						
-					for k in range(broj2 % 10):
-						print("/", end="")
-						canvas= tkinter.Canvas(top, width=20, height=20)
-						canvas.grid(row=2+i, column=k)
-						#canvas.configure(background="red")
-						canvas.configure(background="red")
-				else:			
-				 for l in range(broj2):
-						print("/", end="")
-						canvas= tkinter.Canvas(top, width=20, height=20)
-						canvas.grid(row=1, column=l)
-						#canvas.configure(background="red")
-						canvas.configure(background="red")
+
+def interpolate2():
+	broj = e.get()
+	global broj2
+	broj2=int(broj)+2
+	labelProba=Label(text="ukupno "+ str(broj2)).grid(row=13, column=2)	
+
 
 
 label_none2=Label(text="").grid(row=11, column=2)
 button_3= Button(root, text="Interpolate colors",  command=interpolate,   width=20)
 
 button_3.grid(row=12, column=2)
+
+
 
 
 
